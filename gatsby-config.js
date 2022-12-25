ad15362aa585c95876853f93fa7f9f24215f378d
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2022-12-25 14:17:39 +0000

    :wrench: update gatsby-config
    update sitemetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,16 +9,13 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `StupidJoon's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `StupidJoon`,
+      summary: `devs`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    siteUrl: `https://stupidjoon.github.io`,
   },
   plugins: [
     `gatsby-plugin-image`,
